baa180715bfa8df23d80438feeb4886d3e1de6b8
Akash Laxman Dixit <your-email@example.com>
2025-04-27 18:10:27 +0530

    Fixed case sensitivity issue for Styles folder

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ClassWeb1/'
-})
+  base: '/ClassWeb1/'  // Adjust this if the project is hosted in a subfolder
+});
+
